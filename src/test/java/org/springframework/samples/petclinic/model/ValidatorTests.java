bb1cffc6397bcdf9b49d986ce923b132e929c546
Thomas Fahey <tfahey@MacBook-Pro-2.local>
2021-07-28 13:39:19 -0400

    break a test again

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("notempty");
+		person.setFirstName("notemptyname");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
